700a491e1c2bd47651b751cc8c62510216c5c433
daihiraoka <dai@hiraoka-llc.co.jp>
2020-06-12 04:41:38 +0900

    first commit

---
+++ src/main/java/dev/appsody/starter/Appointment.java
@@ -0,0 +1,110 @@
+package dev.appsody.starter;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.json.bind.annotation.JsonbProperty;
+
+
+/**
+ * The persistent class for the Appointments database table.
+ * 
+ */
+@Entity
+@Table(name="Appointments")
+@NamedQuery(name="Appointment.findAll", query="SELECT a FROM Appointment a")
+
+/*
+ SELECT org.state, org.postcode, org.city, org.name, org.address, 
+		pat.first_name, pat.last_name, app.date, app.time, app.patient_id, prov.name, prov.speciality 
+		FROM Providers prov, Appointments app, Patients pat, Organizations org 
+		WHERE app.provider_id=prov.organization_id 
+		AND app.patient_id="bd2b67d0-f318-471d-83a6-1b130673d9f3" 
+		AND pat.patient_id="bd2b67d0-f318-471d-83a6-1b130673d9f3" 
+		AND org.organization_id=app.provider_id;
+
+		Constructor field order for use in NamedQuery:
+
+		public AppointmentList(String patient_id, String first_name, String last_name, String date, String time,
+            String doc_name, String field, String office_name, String office_addr, String office_city, String office_state,
+            String office_zip) {
+*/
+@NamedQuery(name="Appointment.getAppointments",
+		query="SELECT NEW com.ibm.examplehealth.AppointmentList(p.patientId, p.firstName, p.lastName, app.date, app.time, " + 
+		"prov.name, prov.speciality, org.name, org.address, org.city, org.state, org.postcode) "  + 
+		"FROM Patient p, Appointment app, Provider prov, Organization org " + 
+		"WHERE app.patientId=:pid AND p.patientId=:pid AND app.providerId=prov.organizationId AND org.organizationId=app.providerId")
+
+public class Appointment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@JsonbProperty("Id")
+	@Column(name="appointment_id")
+	private String appointmentId;
+
+	@JsonbProperty("START")
+	private String date;
+
+	private String time;
+
+	@JsonbProperty("PATIENT")
+	@Column(name="patient_id")
+	private String patientId;
+
+	@JsonbProperty("PROVIDER")
+	@Column(name="provider_id")
+	private String providerId;
+
+	public Appointment() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAppointmentId() {
+		return this.appointmentId;
+	}
+
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return this.time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getPatientId() {
+		return this.patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getProviderId() {
+		return this.providerId;
+	}
+
+	public void setProviderId(String providerId) {
+		this.providerId = providerId;
+	}
+}
---
+++ src/main/java/dev/appsody/starter/AppointmentList.java
@@ -0,0 +1,150 @@
+package dev.appsody.starter;
+import javax.json.bind.annotation.JsonbProperty;
+
+/*
+JSON response format:
+
+  {
+    "PATIENTID": 1,
+    "FIRSTNAME": "Ralph               ",
+    "LASTNAME": "Dalmeida            ",
+    "APPT_DATE": "2019-02-21",
+    "APPT_TIME": "02.00.00",
+    "DR_NAME": "Aaron Stone                   ",
+    "MED_FIELD": "Internal Medicine   ",
+    "OFF_NAME": "Toronto Medical               ",
+    "OFF_ADDR": "555 14th Street               ",
+    "OFF_CITY": "Toronto             ",
+    "OFF_STATE": "ON",
+    "OFF_ZIP": "M5H 1T1   "
+},
+ */
+
+ public class AppointmentList {
+
+    private String PATIENTID;
+    private String FIRSTNAME;
+    private String LASTNAME;
+    private String APPT_DATE;
+    private String APPT_TIME;
+    private String DR_NAME;
+    private String MED_FIELD;
+    private String OFF_NAME;
+    private String OFF_ADDR;
+    private String OFF_CITY;
+    private String OFF_STATE;    
+    private String OFF_ZIP;
+
+    public AppointmentList(String patient_id, String first_name, String last_name, String date, String time,
+            String doc_name, String field, String office_name, String office_addr, String office_city, String office_state,
+            String office_zip) {
+        PATIENTID = patient_id;
+        FIRSTNAME = first_name;
+        LASTNAME = last_name;
+        APPT_DATE = date;
+        APPT_TIME = time;
+        DR_NAME = doc_name;
+        MED_FIELD = field;
+        OFF_NAME = office_name;
+        OFF_ADDR = office_addr;
+        OFF_CITY = office_city;
+        OFF_STATE = office_state;
+        OFF_ZIP = office_zip;
+    }
+
+    public String getPATIENTID() {
+        return PATIENTID;
+    }
+
+    public void setPATIENTID(String pATIENTID) {
+        PATIENTID = pATIENTID;
+    }
+
+    public String getFIRSTNAME() {
+        return FIRSTNAME;
+    }
+
+    public void setFIRSTNAME(String fIRSTNAME) {
+        FIRSTNAME = fIRSTNAME;
+    }
+
+    public String getLASTNAME() {
+        return LASTNAME;
+    }
+
+    public void setLASTNAME(String lASTNAME) {
+        LASTNAME = lASTNAME;
+    }
+
+    public String getAPPT_DATE() {
+        return APPT_DATE;
+    }
+
+    public void setAPPT_DATE(String aPPT_DATE) {
+        APPT_DATE = aPPT_DATE;
+    }
+
+    public String getAPPT_TIME() {
+        return APPT_TIME;
+    }
+
+    public void setAPPT_TIME(String aPPT_TIME) {
+        APPT_TIME = aPPT_TIME;
+    }
+
+    public String getDR_NAME() {
+        return DR_NAME;
+    }
+
+    public void setDR_NAME(String dR_NAME) {
+        DR_NAME = dR_NAME;
+    }
+
+    public String getMED_FIELD() {
+        return MED_FIELD;
+    }
+
+    public void setMED_FIELD(String mED_FIELD) {
+        MED_FIELD = mED_FIELD;
+    }
+
+    public String getOFF_NAME() {
+        return OFF_NAME;
+    }
+
+    public void setOFF_NAME(String oFF_NAME) {
+        OFF_NAME = oFF_NAME;
+    }
+
+    public String getOFF_ADDR() {
+        return OFF_ADDR;
+    }
+
+    public void setOFF_ADDR(String oFF_ADDR) {
+        OFF_ADDR = oFF_ADDR;
+    }
+
+    public String getOFF_CITY() {
+        return OFF_CITY;
+    }
+
+    public void setOFF_CITY(String oFF_CITY) {
+        OFF_CITY = oFF_CITY;
+    }
+
+    public String getOFF_STATE() {
+        return OFF_STATE;
+    }
+
+    public void setOFF_STATE(String oFF_STATE) {
+        OFF_STATE = oFF_STATE;
+    }
+
+    public String getOFF_ZIP() {
+        return OFF_ZIP;
+    }
+
+    public void setOFF_ZIP(String oFF_ZIP) {
+        OFF_ZIP = oFF_ZIP;
+    }
+}
